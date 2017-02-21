4e0676cf9c7031bfc0147ba73337f0181337b1d1
rGodoyOk <correogeneral99@hotmail.com>
2017-02-21 20:35:40 -0300

    Create final20022015.cpp

---
+++ final20022015.cpp
@@ -0,0 +1,71 @@
+mostrar listado de jugadores cuyos idJugador (en forma ascendente) este entre [desde, hasta] y con mas de N goles
+por cada jugador se debe mostrar idJugador, cantidad de goles convertidos y el minuto promedio en el q lo convirtio
+
+#include <iostream>
+#define N 3
+#define CANTJUGADORES 20
+
+using namespace std;
+
+struct Gol {
+	unsigned jugadorId,
+			 partidoId,
+			 minuto;
+};
+
+struct Nodo {
+	tDatos info;
+	Nodo *sgte;
+};
+
+struct tJugadores {
+	int cantGoles;
+	Nodo *ListJug;
+};
+
+bool LeerGol (unFlujo, unGol){
+	+
+	+
+	+
+	+
+	return xxx;
+}
+
+
+int GetIndice (int unId){
+	int x;
+	x = (unId - IDMIN) / 2;
+	return x;
+}
+
+int GetId (int unIndice){
+	int x;
+	x = 2 * unIndice + IDMIN;
+	return x;
+}
+
+void CargarDatosEnMemoria (unArreglo, unFlujo){
+	
+	
+	return;	
+}
+
+
+void ListarJugadores (unArreglo, idDesde, idHasta, cantidadMinimaDeGoles){
+
+	return;	
+}
+
+
+
+int main (){
+	
+	
+	
+	tLista Listas[M];
+	
+	
+	
+	
+	
+}
